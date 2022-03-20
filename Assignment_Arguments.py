37293f08e17162e4821ea2184bdf16ddcc1fa814
dimitrisC137 <100574536+dimitrisC137@users.noreply.github.com>
2022-03-20 13:13:42 +0100

    Update Assignment_Arguments.py

--- Assignment_Arguments.py
+++ Assignment_Arguments.py
@@ -1,16 +1,18 @@
-def greet(name,template):
-    template = f"Hello, {name}!"
-    return template
+#part 1
+def greet(name: str, greeting: str ="Hello, <name>!"):
+    return greeting.replace("<name>", name)
 
-greeting = greet("Jim", "" ) 
-print(greeting)
+print (greet("Jim"))
+print(greet("Bob", "What's up, <name>!"))
 
-celestial_bodies = {
+#part 2
+def force(mass: float , body: str = "earth"):
+    celestial_bodies = {
         "sun" : 274,
         "jupiter" : 24.9,
         "neptune" : 11.1,
         "saturn"  : 10.4,
-        "earth"   : 9.8,  #earth as default??
+        "earth"   : 9.8,
         "uranus"  : 8.9,
         "venus"   : 8.9,
         "mars"    : 3.7,
@@ -18,17 +20,15 @@ celestial_bodies = {
         "moon"    : 1.6,
         "pluto"   : 0.6
         }
-def force(mass,body):
-    calculation = mass * body 
-    return calculation
+    return mass * celestial_bodies[body]
 
-result_0 = force(5.972, celestial_bodies["sun"])
+#part 3
+result_0 = force(5.972)
 print(f"The force is {round(result_0,1)}*10^24 kg")
 
-def pull(m1,m2,d):
-    calculation = (6.674*10**-11*(m1*m2))/d**2
-    return calculation
-
+def pull(m1: float,m2: float ,d: float):
+    gravity = (6.674*10**-11*(m1*m2))/d**2
+    return gravity
 result_1 = pull(800,1500,3**2)
 print(f"the pull is {round(result_1,8)}")
 
